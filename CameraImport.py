810217ff23bf07c05e6c96bbb322d181161b8a19
ThomasMAhern <33540039+ThomasMAhern@users.noreply.github.com>
2021-09-13 22:38:02 +0200

    made things more pythonic
    cut down all the long code to something prettier/more efficien/easier to read

--- CameraImport.py
+++ CameraImport.py
@@ -6,82 +6,51 @@ import shutil
 source = sys.argv[1]
 destination = sys.argv[2]
 
-def get_date_of_photo(photo_path):
-    '''This outputs the date for each photo YYYY-MM-DD 
-    Ensure 'os' and 'time' are imported'''
-    ti_c = os.path.getctime(photo_path) # get time of file
-    c_ti = time.ctime(ti_c) # Convert time in seconds to timestamp
-    ti_m = os.path.getmtime(photo_path)
-    m_ti = time.ctime(ti_m)
-    t_obj = time.strptime(m_ti) # Use timestamp string to create time object/structure
-    t_stamp = time.strftime("%Y-%m-%d", t_obj) # Transform time object to date timestamp
-    return str(t_stamp)
-
-def add_date_2_filename(file):
-    '''This takes a filename and adds the date to the beginning of it.'''
-    return str(get_date_of_photo(file) + "_" + file)
-
-def scantree(path):
-    """Recursively yield DirEntry objects for given directory."""
-    for entry in os.scandir(path):
-        if entry.is_dir(follow_symlinks=False):
-            yield from scantree(entry.path)  
-        else:
-            yield entry
-
-def transfer_which_raws(fromhere, tohere):
-    ''' 
-    compares directories for unimported images. 
-    checks for YYYY-MM-DD_filename.iiq format using creation date of file.
-    outputs list of (filename, filepath)
-    '''
-    print(f"{len(list([i for i in scantree(fromhere) if ('.IIQ' or '.NEF') in i.name]))} images in source")
-    print(f"{len(list([i for i in scantree(tohere) if ('.IIQ' or '.NEF') in i.name]))} images in destination")
-    files_i_somehow_missed = []
-    files_new = []
-    for i in scantree(fromhere):
-        if i.name.endswith('.IIQ' or '.NEF'):
-            if '_' in str(i.name):
-                files_i_somehow_missed.append([i.name, i.path, i.name])
-            else:
-                files_new.append([i.name, i.path, f'{get_date_of_photo(i.path)}_{i.name}'])
-    # list of all raw images in destination
-    there = [i.name for i in scantree(tohere) if ('.IIQ' or '.NEF') in i.name]
-    # list of raw images if they match existing files when standard 'date_name.iiq' scheme is applied
-    new_files_4_import = [iiq for iiq in files_new if iiq[2] not in there]
-    # these are files that were probably named, but for some reason weren't imported
-    forgotton_files = [iiq for iiq in files_i_somehow_missed if iiq[2] not in there]
-    print(f'{len(new_files_4_import)} unimported images')
-    if len(forgotton_files) != 0:
-        print(f'!!! {len(forgotton_files)} forgotton files (date_name.iiq in src, but not dst)!!!')
-#     return files_i_somehow_missed 
-    return new_files_4_import
+def transfer_which_raws(source, destination):
+    total_src_photos = list(set(i for i in scantree(source) if i.name.endswith('.IIQ'))) #images in src
+    total_dst_photos = list(set(i for i in scantree(destination) if i.name.endswith('.IIQ'))) #images in dst
+    uneaten_photos = [i for i in scantree(source) if i.name.endswith('.IIQ') if '_' not in i.name 
+                          if add_date_2_DirEntry_name(i) not in [i.name for i in total_dst_photos]] #not previously imported
+    undigested_photos = [i for i in scantree(source) if i.name.endswith('.IIQ') if '_' in i.name 
+                         if i.name not in [i.name for i in total_dst_photos]] #namechange suggests previous import, but photo not in dst
+    photos_4_import = uneaten_photos + undigested_photos
+#     would_b_duplicates = [i for i in total_src_photos if add_date_2_DirEntry_name(i) in [add_date_2_DirEntry_name(i) for i in total_dst_photos]]
+    print(f'Source: {len(total_src_photos)} images  |  Destination: {len(total_dst_photos)} images')
+    print(f'{len(photos_4_import)} images to import')
+#     print(f'{len(would_b_duplicates)} would-be duplicates')
+#     return would_b_duplicates
+    return photos_4_import
 
 def make_folder_if_needed(unloved_photos):
-    '''makes folders if the don't exist
-    takes output from transfer_which_raws function'''
-    for name, path, new_name in unloved_photos:
-        if not os.path.exists(f'{destination}/{get_date_of_photo(path)}'):
-            os.mkdir(f'{destination}/{get_date_of_photo(path)}')
-            print(f'Had to make {get_date_of_photo(path)} folder')
+    '''takes output from transfer_which_raws function'''
+    for photo in unloved_photos:
+        if not os.path.exists(f'{destination}/{get_date_of_photo(photo.path)}'):
+            os.mkdir(f'{destination}/{get_date_of_photo(photo.path)}')
+            print(f'Had to make {get_date_of_photo(photo.path)} folder')
 
 def copy_files_2_dst_with_newname(unloved_photos):
     '''import shutil, make sure paths are loaded'''
     counter = len(unloved_photos)
-    for name, path, new_name in unloved_photos:
-        shutil.copy2(path, f'{destination}/{get_date_of_photo(path)}/{new_name}')
+    for photo in unloved_photos:
+        shutil.copy2(photo.path, f'{destination}/{get_date_of_photo(photo.path)}/{add_date_2_DirEntry_name(photo)}')
         counter -= 1
-        print(f'{name} copied, {counter} more to go!')
+        print(f'{photo.name} copied, {counter} more to go!')
     print('All files copied!')
+    
+def show_files_4_import(list_of_DirEntries):
+    pretty_view = [f'{add_date_2_DirEntry_name(i)}  |  {i.name}  |  {i.path}' for i in list_of_DirEntries]
+    return pretty_view
 
+def any_files_corrupt()
+    total_src_photos = list(set(i for i in scantree(source) if i.name.endswith('.IIQ'))) #images in src
+    total_dst_photos = list(set(i for i in scantree(destination) if i.name.endswith('.IIQ'))) #images in dst
+    if add_date_2_DirEntry_name(i) 
+    print(f'')
 
 unloved_photos = transfer_which_raws(source, destination)
-
-# shows photos that need to be transferred
-for name, path, new_name in unloved_photos:
-    print(name, '|',  new_name, '|', path)
-
+# show_files_4_import(unloved_photos)
 make_folder_if_needed(unloved_photos)
+copy_files_2_dst_with_newname(unloved_photos)
 
 # # This is copying no matter what, so just commenting it out until I can fix
 # if len(unloved_photos) != 0:
